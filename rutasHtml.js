efa621edcbe2ec9178534790d2b2c45f258f25b6
josue-caballero <josue.caballero.macias@outlook.com>
2021-09-21 22:38:41 -0500

    Update views

--- rutasHtml.js
+++ rutasHtml.js
@@ -4,8 +4,29 @@ const routerPaginas = require("express").Router()
 routerPaginas.get("/home", function(request, response) {
 
     datos = {
-        "mensaje": "Este es un mensaje desde el servidor!",
-        "mensaje2": "Este es un mensaje de respaldo"
+        "listaUsuarios": [
+            {
+                "nombre": "Pepe",
+                "edad": 21,
+                "ci": "12020222323"
+            },
+            {
+                "nombre": "Juana",
+                "edad": 19,
+                "ci": "12020222223"
+            },
+            {
+                "nombre": "Mendoza",
+                "edad": 22,
+                "ci": "12220222323"
+            },
+            {
+                "nombre": "Joel",
+                "edad": 24,
+                "ci": "120330222323"
+            }        
+        ]
+
     }
 
     response.render('index', datos);
